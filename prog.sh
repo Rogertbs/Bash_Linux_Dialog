68540b030a13b636a7500b0a1e22f9d45c91f970
Rogério Santos <35261133+Rogertbs@users.noreply.github.com>
2019-01-07 10:08:39 -0200

    Create prog.sh

---
+++ prog.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+proxima=primeira
+
+#loop
+while :
+do
+ 
+     case "$proxima" in 
+	     primeira) 
+		         proxima=idade
+				 nome=$( dialog --stdout --title 'Cadastro' --inputbox 'Informe o nome:' 0 0 )
+				 ;;
+		    idade)
+			      proxima=tel
+				  anterior=primeira
+				  idade=$( dialog --stdout --title 'Cadastro' --inputbox 'Informe a idade:' 0 0 )
+				  ;;
+		      tel)
+			      proxima=fim
+				  anterior=idade				  
+				  tel=$( dialog --stdout --title 'Cadastro' --inputbox 'Informe o telefone:' 0 0 )                 
+
+if [ -z "$nome" ] && [ -z "$idade" ] && [ -z "$tel" ]
+then
+dialog  --title 'Cadastro' --msgbox "Você não preencheu os campos" 0 0
+else 
+mysql --user=root --password=tbs123 crud <<EOF
+INSERT INTO client (nome, idade, telefone) values ('$nome', '$idade', '$tel');
+EOF
+fi
+				  ;;
+		      fim)			   
+              dialog --msgbox 'Obrigado!' 0 0
+			  dialog --yesno 'Deseja continuar?' 0 0
+			  if [ $? -eq 1 ] 
+			  then
+			     break
+			  else
+			      proxima=primeira
+			  fi
+			  ;;
+				*)
+			  dialog --title 'Desculpe' --msgbox 'Não conheço esta opção' 0 0 
+			  echo "Programa abortado..."
+			  sleep 3
+			  exit
+	 esac
+		
+	 retorno=$?
+	 [ $retorno -eq 1 ] && proxima=$anterior
+	 [ $retorno -eq 255 ] && break
+	
+done
+
